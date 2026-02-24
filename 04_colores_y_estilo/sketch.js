4970a9f954e786301cdc36f86cd3622941d466a6
NicolasSarabia <L24170738@culiacan.tecnm.mx>
2026-02-24 04:56:21 +0000

    iiio

--- 04_colores_y_estilo/sketch.js
+++ 04_colores_y_estilo/sketch.js
@@ -4,8 +4,7 @@ function setup() {
 let x = 300;
 function draw() {
  background(255);
- //noStroke();
- //noFill();
+
  fill(0, 128, 0);
  rect(0, 0, width, height/3);
  circle(x, 60, 40);
@@ -15,4 +14,14 @@ function draw() {
  fill(255, 0, 0);
  rect(0, 2*height/3, width, height/3);
  circle(x, 250, 40);
+
+ strokeWeight(5); 
+  stroke(0);      
+  fill(255, 255, 0);
+  rect(150, 200, 100, 50);
+
+  noFill();     
+  stroke(255, 100, 0); 
+  strokeWeight(3);
+  square(175, 300, 50);
 }
