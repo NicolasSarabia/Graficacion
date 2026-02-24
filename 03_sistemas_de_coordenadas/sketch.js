ed4c9d58af2f87a3ab6a96a6fec4461314499042
NicolasSarabia <L24170738@culiacan.tecnm.mx>
2026-02-24 04:51:32 +0000

    jjj

--- 03_sistemas_de_coordenadas/sketch.js
+++ 03_sistemas_de_coordenadas/sketch.js
@@ -1,11 +1,18 @@
 function setup() {
- createCanvas(600, 400);
+  createCanvas(400, 400); 
 }
-function draw() {
- background(220);
- // Línea vertical central
- line(width/2, 0, width/2, height);
- // Línea horizontal central
- line(0, height/2, width, height/2);
 
+function draw() {
+  background(220); 
+  strokeWeight(10); 
+  point(width / 2, height / 2);
+
+  strokeWeight(1); 
+  circle(0, 0, 50);          
+  circle(width, 0, 50);      
+  circle(0, height, 50);     
+  circle(width, height, 50); 
+
+  rectMode(CENTER); 
+  rect(width / 2, height / 2, 100, 50);
 }
